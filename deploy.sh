2e5faf8f83cd2caab5a06d34b2b3ab43fe518638
Isidoros Kefakis <ikefakis@gmail.com>
2022-10-17 16:08:46 +0300

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -10,20 +10,15 @@ npm run build
 cd dist
 
 # place .nojekyll to bypass Jekyll processing
-echo > .nojekyll
+#echo > .nojekyll
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
 #git init
 #git checkout -B main
-git add -A
+git add dist -f
 git commit -m 'deploy'
-
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f origin main:gh-pages
+git subtree push --prefix dist origin gh-pages
 
 cd -
